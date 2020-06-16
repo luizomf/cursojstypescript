82b311b57f7ddb4b961d799aa923fe0ea7a68193
Luiz Otavio <luizomf@gmail.com>
2020-06-16 07:19:43 -0300

    Tipo Any

---
+++ src/A0002-tipo-any/A0002-tipo-any.ts
@@ -0,0 +1,8 @@
+// Utilize any apenas em último caso
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('Olá'));
+console.log(showMessage(1));
