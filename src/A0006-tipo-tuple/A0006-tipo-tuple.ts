33d0faf8e99e2d68e92238ee5c9e62695c92d7dc
Luiz Otavio <luizomf@gmail.com>
2020-06-16 18:35:30 -0300

    Tipo Tuple

---
+++ src/A0006-tipo-tuple/A0006-tipo-tuple.ts
@@ -0,0 +1,20 @@
+// Tuple
+const dadosCliente1: readonly [number, string] = [1, 'Luiz'];
+const dadosCliente2: [number, string, string] = [1, 'Luiz', 'Miranda'];
+const dadosCliente3: [number, string, string?] = [1, 'Luiz'];
+const dadosCliente4: [number, string, ...string[]] = [1, 'Luiz', 'Miranda'];
+
+// dadosCliente1[0] = 100;
+// dadosCliente1[1] = 'Carlos';
+
+console.log(dadosCliente1);
+console.log(dadosCliente2);
+console.log(dadosCliente3);
+console.log(dadosCliente4);
+
+// readonly array
+const array1: readonly string[] = ['Luiz', 'Otávio'];
+const array2: ReadonlyArray<string> = ['Luiz', 'Otávio'];
+
+console.log(array1);
+console.log(array2);
