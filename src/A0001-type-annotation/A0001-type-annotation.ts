f0d348ae515b6bea6f054b0b51f2046f7b51be22
Luiz Otavio <luizomf@gmail.com>
2020-06-16 06:39:49 -0300

    Type Annotations

---
+++ src/A0001-type-annotation/A0001-type-annotation.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+// Tipos básicos (aqui ocorre inferência de tipos)
+let nome: string = 'Luiz'; // Qualquer tipo de strings: '' "" ``
+let idade: number = 0b1010; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
+let adulto: boolean = true; // true ou false
+let simbolo: symbol = Symbol('qualquer-symbol'); // symbol
+// let big: bigint = 10n; // bigint
+
+// Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+let arrayDeNumeros2: number[] = [1, 2, 3];
+let arrayDeStrings: Array<string> = ['a', 'b'];
+let arrayDeStrings2: string[] = ['a', 'b'];
+
+// Objetos
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+  idade: 30,
+  nome: 'Luiz'
+};
+
+// Funções
+function soma(x: number, y: number): number {
+  return x + y;
+}
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import funcao from './mod';
-
-funcao();
