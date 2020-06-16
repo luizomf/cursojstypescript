532452cd9153ee5555e9de627c715be516ceeb24
Luiz Otavio <luizomf@gmail.com>
2020-06-16 17:12:48 -0300

    Tipo Array

---
+++ src/A0005-tipo-array/A0005-tipo-array.ts
@@ -0,0 +1,20 @@
+// Array<T> - T[]
+export function multiplicaArgs(...args: Array<number>): number {
+  return args.reduce((ac, valor) => ac * valor, 1);
+}
+
+export function concatenaString(...args: string[]): string {
+  return args.reduce((ac, valor) => ac + valor);
+}
+
+export function toUpperCase(...args: string[]): string[] {
+  return args.map((valor) => valor.toUpperCase());
+}
+
+const result = multiplicaArgs(1, 2, 3);
+const concatenacao = concatenaString('a', 'b', 'c');
+const upper = toUpperCase('a', 'b', 'c');
+
+console.log(result);
+console.log(concatenacao);
+console.log(upper);
