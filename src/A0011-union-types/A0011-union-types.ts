81373a256b4eb7910be68601ccc0d296c282986a
Luiz Otavio <luizomf@gmail.com>
2020-06-17 12:22:09 -0300

    Union types

--- src/A0011-union-types/A0011-union-types.ts
+++ src/A0011-union-types/A0011-union-types.ts
@@ -1,8 +1,7 @@
 function addOrConcat(
   a: number | string | boolean,
   b: number | string | boolean,
-): string | number {
-  if (typeof a === 'string' && typeof b === 'string') return a + b;
+): number | string {
   if (typeof a === 'number' && typeof b === 'number') return a + b;
   return `${a}${b}`;
 }
@@ -11,3 +10,4 @@ console.log(addOrConcat(10, 20));
 console.log(addOrConcat('10', '20'));
 console.log(addOrConcat(10, '20'));
 console.log(addOrConcat('10', 20));
+console.log(addOrConcat(true, true));
