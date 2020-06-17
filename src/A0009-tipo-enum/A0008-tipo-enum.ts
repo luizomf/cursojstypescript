3dec1c4b52cc055aeeaacdb12a0a158caaa42533
Luiz Otavio <luizomf@gmail.com>
2020-06-17 02:44:30 -0300

    Tipo unknown

---
+++ src/A0009-tipo-enum/A0008-tipo-enum.ts
@@ -0,0 +1,17 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(123456);
---
+++ src/A0010-tipo-unknown/A0010-tipo-unknown.ts
@@ -0,0 +1,9 @@
+let x: unknown;
+
+x = 100;
+x = 'Luiz';
+x = 900;
+x = 10;
+const y = 800;
+
+if (typeof x === 'number') console.log(x + y);
