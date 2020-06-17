38f7477db222fd448fca33121e377a0cb51ce6f4
Luiz Otavio <luizomf@gmail.com>
2020-06-17 17:02:56 -0300

    Type alias

---
+++ src/A0013-type-alias/A0013-type-alias.ts
@@ -0,0 +1,23 @@
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: string;
+};
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+const pessoa: Pessoa = {
+  idade: 30,
+  nome: 'Luiz',
+  salario: 200_000,
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa, 'Azul'));
+console.log(pessoa);
