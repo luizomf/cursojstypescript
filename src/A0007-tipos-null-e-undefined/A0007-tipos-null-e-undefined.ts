4e7a7e5727138c7ff7fa9e5de49293d7074507c2
Luiz Otavio <luizomf@gmail.com>
2020-06-16 23:19:37 -0300

    Tipos null e undefined

---
+++ src/A0007-tipos-null-e-undefined/A0007-tipos-null-e-undefined.ts
@@ -0,0 +1,29 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: any): number | null {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoString = squareOf('2');
+
+if (squareOfTwoString === null) {
+  console.log('Conta inválida');
+} else {
+  console.log(squareOfTwoString * 100);
+}
