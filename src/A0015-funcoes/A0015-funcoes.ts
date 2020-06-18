c5d81d1c4369d9c80c522887140b235e48cc7137
Luiz Otavio <luizomf@gmail.com>
2020-06-17 22:01:52 -0300

    Funçoes como tipo

---
+++ src/A0015-funcoes/A0015-funcoes.ts
@@ -0,0 +1,20 @@
+type MapStringsCallback = (item: string) => string;
+
+export function mapStrings(
+  array: string[],
+  callbackfn: MapStringsCallback,
+): string[] {
+  const newArray: string[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    newArray.push(callbackfn(item));
+  }
+
+  return newArray;
+}
+
+const abc = ['a', 'b', 'c'];
+const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
+console.log(abc);
+console.log(abcMapped);
