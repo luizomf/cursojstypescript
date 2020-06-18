6a0fee7846b46faf87df643814fb066820b9baef
Luiz Otavio <luizomf@gmail.com>
2020-06-18 11:55:51 -0300

    Structural Typing

---
+++ src/A0016-structural-typing/A0016-structural-typing.ts
@@ -0,0 +1,13 @@
+type VerifyUserFn = (user: User, sentValue: User) => boolean;
+type User = { username: string; password: string };
+
+const verifyUser: VerifyUserFn = (user, sentValue) => {
+  return (
+    user.username === sentValue.username && user.password === sentValue.password
+  );
+};
+
+const bdUser = { username: 'joao', password: '123456' };
+const sentUser = { username: 'joao', password: '123456', permissions: '' };
+const loggedIn = verifyUser(bdUser, sentUser);
+console.log(loggedIn);
