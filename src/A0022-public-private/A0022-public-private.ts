96ca9cb793a785091c205a7cf3d7edeb2ad98b48
Luiz Otavio <luizomf@gmail.com>
2020-06-22 04:04:57 -0300

    Modificadores public e private

---
+++ src/A0022-public-private/A0022-public-private.ts
@@ -0,0 +1,36 @@
+export class Empresa {
+  readonly nome: string; // public não necessário
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Luiz', 'Otávio');
+const colaborador2 = new Colaborador('Maria', 'Miranda');
+const colaborador3 = new Colaborador('João', 'Vieira');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+console.log(empresa1);
