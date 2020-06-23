6b036973c185420d5667ae13d1770e3a8c09a07b
Luiz Otavio <luizomf@gmail.com>
2020-06-23 16:07:35 -0300

    Agregaçao

---
+++ src/A0031-agregacao/A0031-agregacao.ts
@@ -0,0 +1,30 @@
+export class CarrinhoDeCompras {
+  private readonly produtos: Produto[] = [];
+
+  inserirProdutos(...produtos: Produto[]): void {
+    for (const produto of produtos) {
+      this.produtos.push(produto);
+    }
+  }
+
+  quantidadeProdutos(): number {
+    return this.produtos.length;
+  }
+
+  valorTotal(): number {
+    return this.produtos.reduce((soma, produto) => soma + produto.preco, 0);
+  }
+}
+
+export class Produto {
+  constructor(public nome: string, public preco: number) {}
+}
+
+const produto1 = new Produto('Camiseta', 49.9);
+const produto2 = new Produto('Caneca', 1.9);
+const produto3 = new Produto('Caneta', 0.9);
+
+const carrinhoDeCompras = new CarrinhoDeCompras();
+carrinhoDeCompras.inserirProdutos(produto1, produto2, produto3);
+console.log(carrinhoDeCompras.valorTotal());
+console.log(carrinhoDeCompras.quantidadeProdutos());
