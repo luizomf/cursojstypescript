33cd2ecf348f19d2ecea464315787aa544e3b02e
Luiz Otavio <luizomf@gmail.com>
2020-06-23 19:17:44 -0300

    Composiçao

---
+++ src/A0032-composicao/A0032-composicao.ts
@@ -0,0 +1,44 @@
+export class Carro {
+  private readonly motor = new Motor();
+
+  ligar(): void {
+    this.motor.ligar();
+  }
+
+  acelerar(): void {
+    this.motor.acelerar();
+  }
+
+  parar(): void {
+    this.motor.parar();
+  }
+
+  desligar(): void {
+    this.motor.desligar();
+  }
+}
+
+export class Motor {
+  ligar(): void {
+    console.log('Motor está ligado...');
+  }
+
+  acelerar(): void {
+    console.log('Motor está acelerando...');
+  }
+
+  parar(): void {
+    console.log('Motor está parado...');
+  }
+
+  desligar(): void {
+    console.log('Motor está desligado...');
+  }
+}
+
+const carro = new Carro();
+
+carro.ligar();
+carro.acelerar();
+carro.parar();
+carro.desligar();
