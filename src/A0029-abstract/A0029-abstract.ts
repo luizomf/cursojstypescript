02a0ea8f2c9e5df3b15c88e949d1105a991207e0
Luiz Otavio <luizomf@gmail.com>
2020-06-23 05:19:32 -0300

    abstract

---
+++ src/A0029-abstract/A0029-abstract.ts
@@ -0,0 +1,48 @@
+export abstract class Personagem {
+  protected abstract emoji: string;
+
+  constructor(
+    protected nome: string,
+    protected ataque: number,
+    protected vida: number,
+  ) {}
+
+  atacar(personagem: Personagem): void {
+    this.bordao();
+    personagem.perderVida(this.ataque);
+  }
+
+  perderVida(forcaAtaque: number): void {
+    this.vida -= forcaAtaque;
+    console.log(
+      `${this.emoji} - ${this.nome} agora tem ${this.vida} de vida...`,
+    );
+  }
+
+  abstract bordao(): void;
+}
+
+export class Guerreira extends Personagem {
+  protected emoji = '\u{1F9DD}';
+
+  bordao(): void {
+    console.log(this.emoji + ' GUERREIRAAAAAA AOOOOOO ATAAAQUEEE!!');
+  }
+}
+export class Monstro extends Personagem {
+  protected emoji = '\u{1F9DF}';
+
+  bordao(): void {
+    console.log(this.emoji + ' MONNNNNNNNNNNSSSTERRRRRRRRRRRRRR!!!!');
+  }
+}
+
+const guerreira = new Guerreira('Guereira', 100, 1000);
+const monstro = new Monstro('Monstro', 87, 1000);
+
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
+monstro.atacar(guerreira);
+monstro.atacar(guerreira);
