3fe2104315d27a478659ff10285ee82740562679
Luiz Otavio <luizomf@gmail.com>
2020-06-24 01:06:51 -0300

    Interfaces

---
+++ src/A0034-interfaces/A0034-interfaces.ts
@@ -0,0 +1,34 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'Luiz',
+  sobrenome: 'Agora tá ok',
+};
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
