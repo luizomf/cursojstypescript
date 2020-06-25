da890c4b18c0be82306d8f482a59d2c91b6c442b
Luiz Otavio <luizomf@gmail.com>
2020-06-25 04:39:42 -0300

    Chaves em tipos

---
+++ src/A0039-chaves-em-tipos/A0039-chaves-em-tipos.ts
@@ -0,0 +1,18 @@
+type Veiculo = {
+  marca: string;
+  ano: number;
+};
+
+type Car = {
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'Ford',
+  year: 2020,
+  name: 'Nome',
+};
+
+console.log(carro);
