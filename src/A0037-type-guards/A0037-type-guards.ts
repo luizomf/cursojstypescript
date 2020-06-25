724552c62fe5e7b77a7805197599bc63c483bcae
Luiz Otavio <luizomf@gmail.com>
2020-06-24 22:10:27 -0300

    Type guards

---
+++ src/A0037-type-guards/A0037-type-guards.ts
@@ -0,0 +1,31 @@
+export function add(a: unknown, b: unknown): number | string {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}`;
+}
+
+console.log(add(1, 5));
+console.log(add('a', 'b'));
+
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  tipo: 'pessoa' = 'pessoa';
+  constructor(public nome: string) {}
+}
+
+function mostraNome(obj: PessoaOuAnimal): void {
+  // if ('nome' in obj) console.log(obj.nome);
+  // if (obj instanceof Aluno) console.log(obj.nome);
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      return;
+    case 'animal':
+      console.log('Isso é um animal', obj.cor);
+      return;
+  }
+}
+
+mostraNome(new Aluno('João'));
+mostraNome({ tipo: 'animal', cor: 'Rosa' });
