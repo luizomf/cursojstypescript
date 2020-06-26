06505dccc41acc697d0fa3ff3926aa7046beba17
Luiz Otavio <luizomf@gmail.com>
2020-06-25 21:20:55 -0300

    Generics #2

---
+++ src/A0044-generics-2/A0044-generics-2.ts
@@ -0,0 +1,19 @@
+type MeuTipo = number;
+
+const arrayNumeros: Array<number> = [1, 2, 3, 4, 5, 6];
+console.log(arrayNumeros);
+
+async function promiseAsync() {
+  return 1;
+}
+
+function minhaPromise(): Promise<MeuTipo | number> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 1000);
+  });
+}
+
+promiseAsync().then((resultado) => console.log(resultado + 1));
+minhaPromise().then((resultado) => console.log(resultado + 1));
