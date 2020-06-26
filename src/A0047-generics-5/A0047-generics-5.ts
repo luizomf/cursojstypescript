4e427666f2c91c6cd6aff3f8bcc645222081ab52
Luiz Otavio <luizomf@gmail.com>
2020-06-26 11:14:14 -0300

    Generics

---
+++ src/A0047-generics-5/A0047-generics-5.ts
@@ -0,0 +1,47 @@
+export class Pessoa<T, U> {
+  constructor(public nome: T, public idade: U) {}
+}
+
+export class Pilha<T> {
+  private contador = 0;
+  private elementos: { [k: number]: T } = {};
+
+  push(elemento: T): void {
+    this.elementos[this.contador] = elemento;
+    this.contador++;
+  }
+
+  pop(): T | void {
+    if (this.estaVazia()) return undefined;
+
+    this.contador--;
+    const elemento = this.elementos[this.contador];
+    delete this.elementos[this.contador];
+    return elemento;
+  }
+
+  estaVazia(): boolean {
+    return this.contador === 0;
+  }
+
+  tamanho(): number {
+    return this.contador;
+  }
+
+  mostrarPilha(): void {
+    for (const chave in this.elementos) {
+      console.log(this.elementos[chave]);
+    }
+  }
+}
+
+const pilha = new Pilha<number | string>();
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+pilha.push(4);
+pilha.push('string');
+
+while (!pilha.estaVazia()) {
+  console.log(pilha.pop());
+}
