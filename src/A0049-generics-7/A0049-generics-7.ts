b87494f5bd954629a265e91a3741615acfbfdb4c
Luiz Otavio <luizomf@gmail.com>
2020-06-27 03:23:58 -0300

    Generics

--- src/A0049-generics-7/A0049-generics-7.ts
+++ src/A0049-generics-7/A0049-generics-7.ts
@@ -1,18 +1,66 @@
-export function isNumber(value: unknown): value is number {
-  return typeof value === 'number';
+// Record
+const objeto1: Record<string, string | number> = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+console.log(objeto1);
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
+
+// Required
+type PessoaRequired = Required<PessoaProtocol>;
+// Partial
+type PessoaPartial = Partial<PessoaRequired>;
+// Readonly
+type PessoaReadonly = Readonly<PessoaRequired>;
+// Pick
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaRequired = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+console.log(objeto2);
+
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
+
+//
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+  sobrenome: string;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'asfd9p8a7sdf90a8s76f9as',
+  nome: 'Luiz',
+  idade: 30,
+  sobrenome: 'Miranda',
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
 }
 
-export function soma<T>(...args: T[]): number {
-  const retorno = args.reduce((sum, value) => {
-    if (isNumber(sum) && isNumber(value)) {
-      return sum + value;
-    }
-    return sum;
-  }, 0);
+const accountApi = mapAccount(accountMongo);
+console.log('API:');
+console.log(accountApi);
 
-  return retorno;
-}
-
-console.log(soma(1, 2, 3));
-console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1]));
-console.log(soma('a', 'b', 'c'));
+// Module mode
+export default 1;
