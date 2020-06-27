1b568aa6aab06fcd6a138da9095f81c639f534f0
Luiz Otavio <luizomf@gmail.com>
2020-06-26 23:04:19 -0300

    Generics

--- src/A0047-generics-5/A0047-generics-5.ts
+++ src/A0047-generics-5/A0047-generics-5.ts
@@ -1,47 +1,9 @@
-export class Pessoa<T, U> {
-  constructor(public nome: T, public idade: U) {}
+export function unirObjetos<T, U>(obj1: T, obj2: U): T & U {
+  // return { ...obj1, ...obj2 };
+  return Object.assign({}, obj1, obj2);
 }
 
-export class Pilha<T> {
-  private contador = 0;
-  private elementos: { [k: number]: T } = {};
-
-  push(elemento: T): void {
-    this.elementos[this.contador] = elemento;
-    this.contador++;
-  }
-
-  pop(): T | void {
-    if (this.estaVazia()) return undefined;
-
-    this.contador--;
-    const elemento = this.elementos[this.contador];
-    delete this.elementos[this.contador];
-    return elemento;
-  }
-
-  estaVazia(): boolean {
-    return this.contador === 0;
-  }
-
-  tamanho(): number {
-    return this.contador;
-  }
-
-  mostrarPilha(): void {
-    for (const chave in this.elementos) {
-      console.log(this.elementos[chave]);
-    }
-  }
-}
-
-const pilha = new Pilha<number | string>();
-pilha.push(1);
-pilha.push(2);
-pilha.push(3);
-pilha.push(4);
-pilha.push('string');
-
-while (!pilha.estaVazia()) {
-  console.log(pilha.pop());
-}
+const obj1 = { chave1: 'valor1' };
+const obj2 = { chave2: 'valor2' };
+const uniao = unirObjetos(obj1, obj2);
+console.log(uniao);
