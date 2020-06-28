b6c2a6a01aa1904ded385156a5518a007515c1a8
Luiz Otavio <luizomf@gmail.com>
2020-06-28 01:58:33 -0300

    Decorator de classes

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,24 @@
+@decorator
+export class Animal {
+  constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    nome: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('Luiz', 'roxo');
+console.log(animal);
