6f5fbfda1b7957ef8f8f11a9e750052d88cae197
Luiz Otavio <luizomf@gmail.com>
2020-06-30 17:29:22 -0300

    Namespaces

--- src/A0060-namespaces/index.ts
+++ src/A0060-namespaces/index.ts
@@ -1,22 +1,4 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-namespace MeuNamespace {
-  export const nomeDoNamespace = 'Luiz';
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="modulo/module.ts" />
 
-  export class PessoaDoNamespace {
-    constructor(public nome: string) {}
-  }
-
-  const pessoaDoNamespace = new PessoaDoNamespace('Luiz');
-  console.log(pessoaDoNamespace);
-
-  export namespace OutroNamespace {
-    export const nomeDoNamespace = 'Nome no outro namespace';
-  }
-}
-
-const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Luiz');
-console.log(pessoaDoNamespace);
 console.log(MeuNamespace.nomeDoNamespace);
-console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
-
-export default 1;
---
+++ src/A0060-namespaces/modulo/module.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace MeuNamespace {
+  export const nomeDoNamespace = 'Luiz';
+
+  export class PessoaDoNamespace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoaDoNamespace = new PessoaDoNamespace('Luiz');
+
+  export namespace OutroNamespace {
+    export const nomeDoNamespace = 'Nome no outro namespace';
+  }
+}
