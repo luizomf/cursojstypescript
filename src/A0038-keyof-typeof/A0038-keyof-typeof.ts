64fdbda17de8458f061b523839ddcae169fa1048
Luiz Otavio <luizomf@gmail.com>
2020-07-31 14:28:31 -0300

    A0038

---
+++ src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -0,0 +1,17 @@
+type CoresObj = typeof coresObj;
+type CoresChaves = keyof CoresObj;
+
+const coresObj = {
+  vermelho: 'red',
+  verde: 'green',
+  azul: 'blue',
+  roxo: 'purple',
+};
+
+function traduzirCor(cor: CoresChaves, cores: CoresObj) {
+  return cores[cor];
+}
+
+console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('verde', coresObj));
+console.log(traduzirCor('roxo', coresObj));
