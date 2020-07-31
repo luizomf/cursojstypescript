eb458b7d2e3ee46984988eae5ce1eb126d96f00f
Luiz Otavio <luizomf@gmail.com>
2020-07-31 14:21:47 -0300

    A0037

--- src/A0037-type-guards/A0037-type-guards.ts
+++ src/A0037-type-guards/A0037-type-guards.ts
@@ -1,17 +1,31 @@
-type CoresObj = typeof coresObj;
-type CoresChaves = keyof CoresObj;
-
-const coresObj = {
-  vermelho: 'red',
-  verde: 'green',
-  azul: 'blue',
-  roxo: 'purple',
-};
-
-function traduzirCor(cor: CoresChaves, cores: CoresObj) {
-  return cores[cor];
+export function add(a: unknown, b: unknown): number | string {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}`;
 }
 
-console.log(traduzirCor('vermelho', coresObj));
-console.log(traduzirCor('verde', coresObj));
-console.log(traduzirCor('roxo', coresObj));
+console.log(add(1, 5));
+console.log(add('a', 'b'));
+
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  tipo: 'pessoa' = 'pessoa';
+  constructor(public nome: string) {}
+}
+
+export function mostraNome(obj: PessoaOuAnimal): void {
+  // if ('nome' in obj) console.log(obj.nome);
+  // if (obj instanceof Aluno) console.log(obj.nome);
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      return;
+    case 'animal':
+      console.log('Isso é um animal', obj.cor);
+      return;
+  }
+}
+
+mostraNome(new Aluno('João'));
+mostraNome({ tipo: 'animal', cor: 'rosa' });
